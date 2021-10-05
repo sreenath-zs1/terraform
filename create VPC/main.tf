b45c72a28feba5d4f450eaea7fdcaaf65b41fefa
Sreenath Rangana <sreenath@zelarsoft.com>
2021-10-05 14:53:47 +0530

    vpc vars pass

--- create VPC/main.tf
+++ create VPC/main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 }
 
 resource "aws_vpc" "vpc_1" {
-    cidr_block = "10.0.0.0/16"
+    cidr_block = "10.0.0.0/16"  #var.cidr_block
     instance_tenancy = "default"
     
     tags = {
