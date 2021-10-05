c84447fa28ca570bc64fad914a45f7ecf582e8b2
Sreenath Rangana <sreenath@zelarsoft.com>
2021-10-05 21:22:34 +0530

    ss

--- create VPC/NAT.tf
+++ create VPC/NAT.tf
@@ -4,7 +4,7 @@ resource "aws_eip" "nat" {
 
 resource "aws_nat_gateway" "nat_gw" {
   allocation_id = "${aws_eip.nat.id}"
-  subnet_id     = "${aws_subnet.public_1.id}"
+  subnet_id     = "${aws_subnet.public-1.id}"
   depends_on    = [aws_internet_gateway.gw]
 }
 
