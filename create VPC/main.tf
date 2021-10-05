253629f754f38e5c47edb56fbe2989f6380b2f9c
Sreenath Rangana <sreenath@zelarsoft.com>
2021-10-05 14:44:01 +0530

    public and private subnets

--- create VPC/main.tf
+++ create VPC/main.tf
@@ -22,3 +22,63 @@ resource "aws_vpc" "vpc_1" {
         Name = "My VPC1-sree"
     }
 }
+resource "aws_subnet" "public-1" {
+  vpc_id     = aws_vpc.vpc_1.id
+  cidr_block = "10.0.101.0/24"
+  availability_zone ="us-east-1a"
+ 
+  tags = {
+    Name = "public subnet-1"
+  }
+}
+
+resource "aws_subnet" "public-2" {
+  vpc_id     = aws_vpc.vpc_1.id
+  cidr_block = "10.0.102.0/24"
+  availability_zone ="us-east-1b"
+  
+  tags = {
+    Name = "public subnet-2"
+  }
+}
+
+resource "aws_subnet" "public-3" {
+  vpc_id     = aws_vpc.vpc_1.id
+  cidr_block = "10.0.103.0/24"
+  availability_zone ="us-east-1c"
+  
+  tags = {
+    Name = "public subnet-3"
+  }
+}
+
+
+resource "aws_subnet" "private-1" {
+  vpc_id     = aws_vpc.vpc_1.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone ="us-east-1a"
+ 
+  tags = {
+    Name = "private-subnet-1"
+  }
+}
+
+resource "aws_subnet" "private-2" {
+  vpc_id     = aws_vpc.vpc_1.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone ="us-east-1b"
+  
+  tags = {
+    Name = "private-subnet-2"
+  }
+}
+
+resource "aws_subnet" "private-3" {
+  vpc_id     = aws_vpc.vpc_1.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone ="us-east-1c"
+  
+  tags = {
+    Name = "private-subnet-3"
+  }
+}
